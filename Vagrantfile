6664e206eb3de7cac6d8bbf754faf301f9998037
Phil Sturgeon <email@philsturgeon.co.uk>
2013-11-11 23:05:54 -0500

    Tarted up the run script.

--- Vagrantfile
+++ Vagrantfile
@@ -42,8 +42,6 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Example for VirtualBox:
   #
   config.vm.provider :virtualbox do |vb|
-    # Don't boot with headless mode
-    vb.gui = true
   
     # Use VBoxManage to customize the VM. For example to change memory:
     vb.customize ["modifyvm", :id, "--memory", "1024"]
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+echo NodeJS `node -v` "+ Cheerio"
 time nodejs ./n2-cheerio.js > ./results/n2-cheerio.csv
+echo `php -v | grep "PHP 5" | sed -E 's/-(.*)$//'` "+ phpQuery"
 time php ./p1-phpquery.php > ./results/p1-phpquery.csv
+echo `php -v | grep "PHP 5" | sed -E 's/-(.*)$//'` "+ ReactPHP + phpQuery"
 time php ./p2-async.php > ./results/p2-async.csv
+echo "== Complete =="
